7bc5155189521228bbb0b14857db0c7f94eac73e
inchinaxiaofeng@gmail.com <inchinaxiaofeng@gmail.com>
2026-01-22 17:23:40 +0800

    feat(malloc): Add malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,30 +1,33 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
+// NOTE: 供 `malloc` 和 `free` 使用的
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+static void *addr = NULL;
+#endif
+
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
-void srand(unsigned int seed) {
-  next = seed;
-}
+void srand(unsigned int seed) { next = seed; }
 
-int abs(int x) {
-  return (x < 0 ? -x : x);
-}
+int abs(int x) { return (x < 0 ? -x : x); }
 
-int atoi(const char* nptr) {
+int atoi(const char *nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') {
+    nptr++;
+  }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
@@ -34,12 +37,17 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if (addr == NULL) {
+    addr = heap.start;
+  }
+  void *ptr = addr;
+  addr = (char *)addr + size;
+
+  return ptr;
 #endif
   return NULL;
 }
 
-void free(void *ptr) {
-}
+void free(void *ptr) {}
 
 #endif
