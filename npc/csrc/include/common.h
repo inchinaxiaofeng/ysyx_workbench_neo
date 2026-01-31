ae659b5fd6c5f6de41fda61de823cf98e099f363
inchinaxiaofeng@gmail.com <inchinaxiaofeng@gmail.com>
2026-01-31 11:18:48 +0800

    build: resolve dependency hell and stabilize verilator toolchain

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -16,9 +16,11 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#include <assert.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 #include <autoconfig.h>
